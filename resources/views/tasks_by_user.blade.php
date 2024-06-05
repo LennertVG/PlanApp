7b66aba7d3d43b6c87d44a19a8daa639de2485cd
jopdel <opdelochtjohn@gmail.com>
2024-06-05 17:31:31 +0200

    get tasks by user_id, basic functionality

---
+++ resources/views/tasks_by_user.blade.php
@@ -0,0 +1,25 @@
+@extends('sections.app')
+    @section('content')
+            @if(Auth::user())
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                        <h1>Tasks by user</h1>
+                        @foreach ($tasks as $task)
+                            <ul>
+                                <li>
+                                    <strong>Naam:</strong> {{ $task->name }}<br>
+                                    <strong>Vak ID:</strong> {{ $task->course_id }}<br>
+                                    <strong>Deadline:</strong> {{ $task->formatted_deadline }}<br>
+                                    <strong>Beschrijving:</strong> {{ $task->description }}<br>
+                                    <strong>Gecreëerd door:</strong> {{ $task->created_by }}<br>
+                                    <strong>Taaktype ID:</strong> {{ $task->tasktype_id }}<br>
+                                </li>
+                            </ul>
+                        @endforeach
+                        </div>
+                    </div>
+                </div>
+            @endif
+    @endsection
