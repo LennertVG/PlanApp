24df880f07f74e9e478bf5fb75e4fe1a65b7da68
jopdel <opdelochtjohn@gmail.com>
2024-06-09 20:01:54 +0200

    Fixed bug in ViewComposerController

--- app/Http/Controllers/ViewComposerController.php
+++ app/Http/Controllers/ViewComposerController.php
@@ -17,20 +17,6 @@ class ViewComposerController extends Controller
 
     public function getTasksByUsersForTasks()
     {
-        if (Auth::check()) {
-            $user = Auth::user()->id;
-            $userWithTasks = User::with('tasks.course', 'tasks.tasktype')->find($user);
-            $tasks = $userWithTasks->tasks->map(function ($task) {
-                $task->formatted_deadline = Carbon::parse($task->deadline)->format('d-m-Y');
-                return $task;
-            });
-
-            $courses = Course::all();
-            $taskTypes = TaskType::all();
-
-            return view('your_view', compact('tasks', 'courses', 'taskTypes'));
-        } else {
-            return redirect()->route('login'); // Or return a specific view for non-authenticated users
-        }
+        return view('tasks-by-user', []);
     }
 }
